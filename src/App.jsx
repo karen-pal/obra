918091f389a0d6de3e6fda101c1e0e14be1c44fb
Natali Palacio Pastor <cristinanatalip@gmail.com>
2024-09-21 17:13:45 -0300

    Added text.Added animation to tooltips

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import {
   TooltipContentOne,
 } from "./components/Tooltips";
 import { useEffect, useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 // import CloseIcon from "./assets/CloseIcon.jsx";
 
 export default function Home() {
@@ -30,6 +31,9 @@ export default function Home() {
     <div className="w-[100vw] h-[100vh] overflow-hidden">
       <h1 className="text-center text-3xl mt-4 mb-2">Lenguaje Frontera</h1>
       <h3 className="text-center text-xl">por Karen Palacio</h3>
+      <h6 className="text-center text-sm mt-2 px-4">
+        Esto es una bitácora interactiva, hacé click en distintas partes
+      </h6>
 
       <div className="w-[100vw] h-[100vh] absolute top-[-1.9vh]">
         <Canvas
@@ -83,29 +87,34 @@ export default function Home() {
           Bucles
         </h1>
       </div>
-      {visibleTooltip ? (
-        <div className="w-[100vw] h-[15vh] absolute bottom-[15vh] flex items-center justify-center">
-          <div
-            ref={tooltipRef}
-            className="p-6 bg-zinc-800 w-[95vw] h-[35vh] rounded border-zinc-600 relative overflow-y-auto overflow-x-hidden"
-          >
-            <button
-              className="fixed bottom-[36.5vh] right-[4.5vw] z-50"
-              onClick={() => setVisibleTooltip(false)}
+      <AnimatePresence>
+        {visibleTooltip ? (
+          <div className="w-[100vw] h-[15vh] absolute bottom-[15vh] flex items-center justify-center">
+            <motion.div
+              className="p-6 bg-zinc-800 w-[95vw] h-[35vh] rounded border-zinc-600 relative overflow-y-auto overflow-x-hidden"
+              initial={{ opacity: 0, y: 20 }} // Initial state (hidden)
+              animate={{ opacity: 1, y: 0 }} // Animate to visible
+              exit={{ opacity: 0, y: 20 }} // Animate out when closing
+              transition={{ duration: 0.3 }} // Duration of the animation
             >
-              <CloseIcon fill="#fafafa" size={16} />
-            </button>
-            {tooltipContent}
+              <button
+                className="fixed bottom-[36.5vh] right-[4.5vw] z-50"
+                onClick={() => setVisibleTooltip(false)}
+              >
+                <CloseIcon fill="#fafafa" size={16} />
+              </button>
+              {tooltipContent}
 
-            <button
-              className="bg-zinc-200 hover:bg-zinc-300 py-2 px-4 mt-4 rounded text-zinc-950"
-              onClick={() => setVisibleModal(true)}
-            >
-              Ver más
-            </button>
+              <button
+                className="bg-zinc-200 hover:bg-zinc-300 py-2 px-4 mt-4 rounded text-zinc-950"
+                onClick={() => setVisibleModal(true)}
+              >
+                Ver más
+              </button>
+            </motion.div>
           </div>
-        </div>
-      ) : null}
+        ) : null}
+      </AnimatePresence>
       {visibleModal ? (
         <div className="w-[100vw] h-[100vh] absolute bottom-4 flex items-center justify-center">
           <div className="p-6 bg-zinc-800 w-[95vw] h-[95vh] rounded border-zinc-600 relative ">
